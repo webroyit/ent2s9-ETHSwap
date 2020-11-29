a0946ca975f543514030fe2ce2ead2c5ecc3b89e
royweb <xytank@gmail.com>
2020-11-29 13:09:20 -0500

    Add a contract for Token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,8 @@
+const Token = artifacts.require("Token");
 const ETHSwap = artifacts.require("ETHSwap");
 
 // Put these contracts on the blockchain
 module.exports = function (deployer) {
+  deployer.deploy(Token);
   deployer.deploy(ETHSwap);
 };
