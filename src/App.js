617fef7ed95f1e8997b05ba9e6be9f997ad951f4
royweb <xytank@gmail.com>
2020-12-01 12:23:43 -0500

    Install bootstrap and add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>ETH Swap</h1>
-    </div>
-  );
+import Navbar from './components//Navbar';
+
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <Navbar />
+        <h1 className="text-center mt-5">ETH Swap</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
