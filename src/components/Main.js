fbd3541111fb2b39f75fec6c76b2bc6d5930d95f
royweb <xytank@gmail.com>
2020-12-02 14:47:34 -0500

    Add buttons to toggle between buy and sell form

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 
 import BuyForm from './BuyForm';
+import SellForm from './SellForm';
 
 class Main extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      output: '0'
+      currentForm: 'buy'
     }
   }
 
   render() {
+    let content;
+    if (this.state.currentForm === 'buy') {
+      content = <BuyForm
+        ethBalance={this.props.ethBalance}
+        tokenBalance={this.props.tokenBalance}
+        buyTokens={this.props.buyTokens} />
+    }
+    else {
+      content = <SellForm />
+    }
+
     return (
       <div id="content">
+        <div className="d-flex justify-content-between mb-3">
+          <button
+              className="btn btn-outline-success"
+              onClick={() => {
+                this.setState({ currentForm: 'buy' })
+              }}
+            >
+            Buy
+          </button>
+          <span className="text-muted">&lt; &nbsp; &gt;</span>
+          <button
+              className="btn btn-outline-warning"
+              onClick={() => {
+                this.setState({ currentForm: 'sell' })
+              }}
+            >
+            Sell
+          </button>
+        </div>
+
         <div className="card mb-4" >
           <div className="card-body">
-            <BuyForm
-              ethBalance={this.props.ethBalance}
-              tokenBalance={this.props.tokenBalance}
-              buyTokens={this.props.buyTokens} />
+            { content }
           </div>
         </div>
       </div>
---
+++ src/components/SellForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class SellForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      output: '0'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+          Sell Form
+      </div>
+    );
+  }
+}
+
+export default SellForm;
